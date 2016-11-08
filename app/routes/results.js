a58580dfdb0b016417ad9b0048f50a22c388cd85
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-08 12:17:28 -0800

    Updates API call to show more results per call

--- app/routes/results.js
+++ app/routes/results.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    var url = 'https://api.github.com/search/users?q=location:' + params.location + '+language:' + params.language;
+    var url = 'https://api.github.com/search/users?q=location:' + params.location +  '+followers:>100+language:' + params.language + '?access_token=dc59464a6d2be7507b70311bb89fa84fff4701dd&per_page=100';
     return Ember.$.getJSON(url).then(function(responseJSON){
-      return responseJSON.results;
+      return responseJSON.items;
     });
   }
 });
