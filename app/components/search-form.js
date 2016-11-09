c267d0c5b882217c83f361fc8ba76f98ece35ee2
Marta Sanchez <sanchez.marta@gmail.com>
2016-11-09 10:25:58 -0800

    clean al lthe try and error code

--- app/components/search-form.js
+++ app/components/search-form.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  location: 'Portland',
+  language: 'JavaScript',
   actions: {
     lookUp() {
       var params = {
--- app/routes/results.js
+++ app/routes/results.js
@@ -10,11 +10,9 @@ export default Ember.Route.extend({
       for(var j = 0; j < users.length; j++){
         usernames.push(users[j].login);
       }
-      // console.log(usernames);
       for(var i = 0; i < usernames.length; i++){
         Ember.$.getJSON('https://api.github.com/users/' + usernames[i] + '/repos?&per_page=100&access_token=dc59464a6d2be7507b70311bb89fa84fff4701dd').then(function(responseJSON){
           var repos = JSON.parse(JSON.stringify(responseJSON));
-          // console.log(repos);
           var topRepos = [];
           for (var k = 0; k < repos.length; k++){
             if (repos[k].language === params.language && repos[k].forks_count >= 2 && repos[k].watchers >= 2 && repos[k].stargazers_count >= 2){
@@ -23,11 +21,14 @@ export default Ember.Route.extend({
           }
           if(topRepos.length >= 2){
             candidates.push(topRepos[0].owner.login);
-            // console.log("first " +candidates.length);
-            // return candidates;
+            return candidates;
           }
         });
       }
+
+
+    //  console.log(candidates); //this runs before json terminates
+
       // for (var l = 0; l < candidates.length; l++){
       //
       //   console.log(candidates[l]);
