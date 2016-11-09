099c7f11836ec133b350b7cc36cd146a09b51f38
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-09 15:16:05 -0800

    Begins watchlist service. Commits in order to pull latest content in results.hbs

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  watchList: Ember.inject.service(watch-user),
   model: function(params){
     var url = 'https://api.github.com/search/users?q=location:' + params.location +  '+followers:>100+language:' + params.language + '?access_token=dc59464a6d2be7507b70311bb89fa84fff4701dd&per_page=100';
     var candidatesInformation = [];
@@ -58,5 +59,8 @@ export default Ember.Route.extend({
       }); //end the first when
     }); // end the initial Github API call
     return candidatesInformation;
-  } //end model
+  },
+  actions:{
+
+  }
 }); // end ember export
---
+++ tests/unit/services/watch-user-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:watch-user', 'Unit | Service | watch user', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});
