c1d8efa0c90950f7bc7939f3c80a1b2b1c0ff5c8
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-09 16:15:57 -0800

    Adds helper for emailing users

---
+++ tests/unit/helpers/email-link-test.js
@@ -0,0 +1,12 @@
+
+import { emailLink } from 'github-project/helpers/email-link';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | email link');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = emailLink([42]);
+  assert.ok(result);
+});
+
