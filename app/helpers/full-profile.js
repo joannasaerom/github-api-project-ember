689d08a89f3e9e1d438b980b31689efabdc48069
Joanna Anderson <joanna.saerom.anderson@gmail.com>
2016-11-09 19:50:42 -0800

    saving to pull latest content

--- app/helpers/full-profile.js
+++ app/helpers/full-profile.js
@@ -5,7 +5,7 @@ export function fullProfile(params) {
   if (params[0] === undefined || params[0] === null){
     return new Ember.String.htmlSafe('<span> No Email for Fullcontact </span>');
   } else{
-    return new Ember.String.htmlSafe("{{#link-to 'fullcontact'" + params[0] + "}}Full Profile{{/link-to}}");
+    return "{{#link-to 'fullcontact'" + params[0] + "}}Full Profile{{/link-to}}";
   }
 }
 
